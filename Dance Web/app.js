1c6876e7feccdb784364cbdb231890776c694031
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2022-01-25 21:53:32 +0530

    Create app.js

---
+++ Dance Web/app.js
@@ -0,0 +1,20 @@
+const express=require("Express");
+const path =require("path");
+const fs=require("fs");
+const app=express();
+const port=80;
+//EXPRESS   STUFF
+app.use('/static',express.static('static'));//for serviving static file
+app.use(express.urlencoded());//help in taking data from form
+
+//PUG SPECIFIC STUFF
+app.set('view engine','pug');//set template engine as pug
+app.set('views',path.join(__dirname,'views'));//set the view directory
+app.get('/',(req,res)=>{
+    const con='this is a Dance web site';
+    const params={'title':'Dance WebSite','contant':con};
+    res.status(200).render('index.pug',params);
+})
+app.listen(port,()=>{
+    console.log('live');
+})
