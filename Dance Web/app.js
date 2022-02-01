e44d467f6d609626c988ec027ef79f3b921bccd2
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2022-02-01 23:29:31 +0530

    Update app.js

--- Dance Web/app.js
+++ Dance Web/app.js
@@ -2,7 +2,25 @@ const express=require("Express");
 const path =require("path");
 const fs=require("fs");
 const app=express();
+// const bodyparse=require("body-parser");
 const port=80;
+var mongoose =require("mongoose");
+mongoose.connect('mongodb://localhost/contactDance');
+var db=mongoose.connection;
+
+//defining mongoose schema
+const contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    desc: String
+  });
+
+  const contact = mongoose.model('contact', contactSchema); 
+
+
+
 //EXPRESS   STUFF
 app.use('/static',express.static('static'));//for serviving static file
 app.use(express.urlencoded());//help in taking data from form
@@ -17,6 +35,14 @@ app.get('/',(req,res)=>{
 })
 app.get('/contact',(req,res)=>{
     res.status(200).render('contact.pug');
+})
+app.post('/contact',(req,res)=>{
+    var mydata= new contact(req.body);
+    mydata.save().then(()=>{
+        res.send("your form hase been send in database");
+    }).catch(()=>{
+        res.status(400).send("item was not saved in database");
+    })
 })
 app.listen(port,()=>{
     console.log('live');
